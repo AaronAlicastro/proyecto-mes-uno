7421e5b9dc00d08135950884f9fdd6236fedde1c
Luis Carlos <luiscarlosvaldivieso3@gmail.com>
2022-03-25 17:43:10 -0500

    Agrego js

--- funcion/fuente.js
+++ funcion/fuente.js
@@ -1,13 +1,18 @@
 var m1 = document.querySelector('.m1');
+var m2 = document.querySelector('.m2');
 var mesa1 = document.querySelector('.mesa1');
+var mesa2 = document.querySelector('.mesa2');
 var EstadoMesa;
+var EstadoMesa1;
 var form = document.querySelector('#form');
 var cerrar = document.querySelector("#cerrar");
 var boton = document.querySelector("#submit");
 var nombre = document.querySelector("#nombre")
 var modal = localStorage.getItem('EstadoMesa');
+var apellido = document.querySelector('#apellido');
 
-// if(nombre.value == ''){
+var cajaSecundaria = document.querySelector('#cajaSecundaria');
+o();
 validacion2();
 function validacion2(){
     nombre.addEventListener('input',()=>{
@@ -25,36 +30,32 @@ function validacion2(){
         }
     }); 
 }
-        // nombre.classList.remove('.borde1')
-        // nombre.classList.add('.borde1');
-        // nombre.style.border = "solid 1px red"
-    // });
-// }else{
-    // nombre.addEventListener('blur',()=>{
-        // alert("lleno");
-        // nombre.classList.remove('.borde1');
-        // nombre.classList.add('.borde2');
-        // nombre.style.border = "solid 1px green"
-    // });
-// }
+
 if(modal == 'false'){
     mesa1.style.background = 'red';
     m1.style.color = 'white';
+    mesa2.style.background = 'red';
+    m2.style.color = 'white';
 }
 mesa1.addEventListener('click', () => {
-    var mesa2 = true;
+    var mesa1 = true;
+    mesa1 = localStorage.getItem('EstadoMesa');
 
-    mesa2 = localStorage.getItem('EstadoMesa');
-    datosPersona(mesa2);
-    
+    datosPersona(mesa1,'mesa1');
+    limpiar();
 })
-
-// console.log(typeof mesa2)
-// console.log(mesa2)
-
-function datosPersona(mesa2){
+mesa2.addEventListener('click', () => {
+    var mesa2 = true;
+    mesa2 = localStorage.getItem('EstadoMesa1');
+    // console.log(mesa2);
+    datosPersona(mesa2,'mesa2');
+    limpiar();
+})
+var personas = [];
+var nombres2 = [];
+function datosPersona(mesa,seleccions){
     
-    if(mesa2 == null){
+    if(mesa == null){
         form.classList.remove('form1');
         form.classList.add('form2');
         cerrar.addEventListener('click',()=>{
@@ -63,7 +64,6 @@ function datosPersona(mesa2){
         });
         boton.addEventListener('click',()=>{
             if(nombre.value == '' || apellido.value == ''){
-                // nombre.style.border = "solid 1px red";
                 alert("Datos incompletos");
                 if(nombre.value == ''){
                     nombre.style.border = "solid 1px red";
@@ -73,34 +73,83 @@ function datosPersona(mesa2){
                 }
                 
             }else{
-                form.classList.remove('form2');
-                form.classList.add('form1');
-                mesa1.classList.remove('desocupada');
-                mesa1.classList.add('ocupada');
-                EstadoMesa = false; 
-                localStorage.setItem('EstadoMesa', EstadoMesa);
+                
+                // form.classList.remove('form2');
+                // form.classList.add('form1');
+                // console.log(seleccions.value)
+                if(seleccions == 'mesa1'){
+                    // cajaSecundaria.appendChild(p2);
+                    console.log('es mesa1')
+                    EstadoMesa = false; 
+                    mesa1.classList.remove('desocupada');
+                    mesa1.classList.add('ocupada');
+                    localStorage.setItem('EstadoMesa', EstadoMesa);
+                    form.classList.remove('form2');
+                    form.classList.add('form1');
+                    // var numero = Math.random() * 1000000
+                    // console.log(parseInt(numero));
+                    // var nombreA = nombre.value;
+                    // var apellidoB = apellido.value;
+                    // persona += nombreA;
+                    // persona += apellidoB;
+                    // personas += persona
+                    // localStorage.setItem('personas',personas);
+                    var nombres = nombre.value + apellido.value;
+                
+                    nombres2.push(nombres)
+                    // personas.push(nombres2)
+                    localStorage.setItem('apellidos',nombres2);
+                    
+                    // nombres2 += (nombres)
+                    // personas2()
+                    // var p = document.createElement('p');
+                    // p.innerHTML = localStorage.getItem('apellidos');
+                    // cajaSecundaria.appendChild(p);
+                    // var p = document.createElement('p');
+                    // p.innerHTML = localStorage.getItem('apellidos');
+                    // cajaSecundaria.appendChild(p);
+                    
+                }
+                if(seleccions == 'mesa2'){
+                    mesa2.classList.remove('desocupada');
+                    mesa2.classList.add('ocupada');
+                    EstadoMesa1 = false;
+                    localStorage.setItem('EstadoMesa1', EstadoMesa1); 
+                    form.classList.remove('form2');
+                    form.classList.add('form1');
+                    // personas2() 
+                    // var p = document.createElement('p');
+                    // p.innerHTML = localStorage.getItem('apellidos');
+                    // cajaSecundaria.appendChild(p);
+                    nombres2.push(nombres)
+                    localStorage.setItem('apellidos',nombres2);
+                    // var p = document.createElement('p');
+                    // p.innerHTML = localStorage.getItem('apellidos');
+                    // cajaSecundaria.appendChild(p);
+                }
+               
             }
+            // EstadoMesa = true;
         });
     }else{
         alert('Se ecnuentra temporalmente ocupado');
     }
 };
-// function validacion(){
-//     if(nombre.value == ''){
-//         nombre.addEventListener('active',()=>{
-//             // nombre.style.border = 'solid 1px red';
-//             nombre.classList.add('.borde1')
-//             nombre.classList.remove('.borde2')
-            
-//         });
-//     }else{
-//         nombre.addEventListener('active',()=>{
-//             // nombre.style.border = 'solid 1px green';
-//             nombre.classList.add('.borde2')
-//             nombre.classList.remove('.borde1')
-            
-//         });
-//     }
-    
-// }
-// validacion();
+function limpiar(){
+    nombre.value = '';
+}
+
+
+
+// var p = document.createElement('p');
+// // var p2 = document.createElement('p');
+// var otro = localStorage.getItem('apellidos');
+// console.log(otro);
+// p.innerHTML = otro;
+// // p2.innerHTML = apellido.value;
+// cajaSecundaria.appendChild(p);
+function o(){
+    var p = document.createElement('p');
+    p.innerHTML = localStorage.getItem('apellidos');
+    cajaSecundaria.appendChild(p); 
+}
