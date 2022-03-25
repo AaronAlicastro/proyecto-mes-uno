2cb9f34716a22fd272b96d35211e1100f1452e75
Aaron Alicastro <aalicastro5@gmail.com>
2022-03-25 00:16:43 -0500

    creado la factura y su base logica

---
+++ funcion/funcionPago.js
@@ -0,0 +1,39 @@
+const validarpago = document.querySelector("#validarpago");
+const contenedorFactura = document.querySelector("#contenedorFactura");
+const persona = document.querySelectorAll(".persona");
+const pedir = document.querySelectorAll(".pedir");
+const barraInfo_info = document.querySelectorAll(".barraInfo_info");
+const barraDescripcion_descripcion = document.querySelectorAll(".barraDescripcion_descripcion");
+const barraTotal_total = document.querySelectorAll(".barraTotal_total");
+
+var datosAja = [1,2,3,4,5,6,7,8,9,10,21,432,5,57,79,4,23,4456];
+
+validarpago.addEventListener("click", (e)=>{
+	contenedorFactura.style.display = "block";
+	contenedorFactura.style.marginTop = "10em";
+	contenedorFactura.style.marginRight = "30px";
+	contenedorFactura.style.width = "60%";
+	agregarDatosFactura();
+});
+
+function agregarDatosFactura() {
+	barraDescripcion_descripcion[0].value = "Producto \n";
+	barraDescripcion_descripcion[1].value = "Cantidad \n";
+	barraDescripcion_descripcion[2].value = "Precio \n";
+	let numeroFactura = Math.floor(Math.random()*8000000 + 500);
+	const fecha = new Date();
+	barraInfo_info[0].innerText = `n° factura: ${numeroFactura}`;
+	barraInfo_info[1].innerText = `Nombre: ${persona[0].value}`;
+	barraInfo_info[3].innerText = `Fecha: ${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`;
+	barraInfo_info[2].innerText = `Mail: ${persona[1].value}`;
+	for (let i = 0; i < datosAja.length; i++) {
+		barraDescripcion_descripcion[0].value += "\n"+datosAja[i]+"\n"+"––––––––––––––––––––––––– \n"
+		barraDescripcion_descripcion[1].value += "\n"+datosAja[i]+"\n"+"––––––––––––––––––––––––– \n"
+		barraDescripcion_descripcion[2].value += "\n"+datosAja[i]+"\n"+"––––––––––––––––––––––––– \n";
+	}
+	barraTotal_total[0].value = "Total  –> ";
+	barraTotal_total[1].value = "Aqui va el total";
+	for(let i = 0; i < 6; i++){
+		pedir[i].value = "";
+	}
+}
